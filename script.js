70e54a59a36dc5454a36010cd3bb4fda977b0e23
Oybek <oybekjonxayrullayevich@gmail.com>
2022-04-08 08:38:58 +0500

    yuqori darajali await

--- script.js
+++ script.js
@@ -3,6 +3,15 @@
 // console.log(func.b , func.q,func.p );
 // console.log(func.buyProduct());
 
-import sotibOl from './buy.js';
+// import sotibOl from './buy.js';
 
-console.log(sotibOl('olma', 5));
+// console.log(sotibOl('olma', 5));
+
+console.log('hello');
+async function salom() {}
+salom();
+const country = await fetch('https://restcountries.com/v2/all');
+const countryJson = await country.json();
+console.log(countryJson);
+
+console.log('salom');
